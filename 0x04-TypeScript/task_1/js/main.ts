b466858685b16072f2f153b41df0d69b3c500c7b
sundayconfidencechinecherem <sundayconfidencechinecherem@gmail.com>
2025-11-07 12:59:20 +0100

    update

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,8 +16,8 @@ interface printTeacherFunction{
     (firstName: string, lastName: string): string;
 }
 
-function printTeacher(firstName: string, lastName: string): string  {
-    return `${firstName.charAt(0)}. ${lastName}`;
+function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
+    return `${firstName}. ${lastName}`;
 }
 const teacher3: Teacher = {
 firstName: 'John',
