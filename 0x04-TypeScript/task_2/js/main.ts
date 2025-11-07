5b2703d9021a7ebe3fc61f911329602c4a59e9df
sundayconfidencechinecherem <sundayconfidencechinecherem@gmail.com>
2025-11-07 15:00:47 +0100

    initial commit

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,50 @@
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+class Director {
+
+        workFromHome(): string {
+            return 'Working from home';
+        }
+        getCoffeeBreak(): string {
+            return 'Getting a coffee break';
+        }
+        workDirectorTasks(): string{
+            return 'Getting to director tasks';
+        }
+}
+
+class Teacher {
+    workFromHome(): string {
+        return 'Cannot work from home';
+    }
+    getCoffeeBreak(): string {
+        return 'Cannot have a break';
+    }
+    workTeacherTasks(): string {
+        return 'Getting to work';
+    }
+}
+
+function createEmployee (salary: string | number) : Director | Teacher {
+    if(typeof salary == 'number'  && salary < 500 ){
+        return new Teacher();
+    }
+    else{
+        return new Director();
+    }
+}
+
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
