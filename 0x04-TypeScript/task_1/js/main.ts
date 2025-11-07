e38938f7a363ae9c6178d1b6762082fa5bbb5021
sundayconfidencechinecherem <sundayconfidencechinecherem@gmail.com>
2025-11-07 13:32:26 +0100

    update

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,6 +16,16 @@ interface printTeacherFunction{
     (firstName: string, lastName: string): string;
 }
 
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+
 function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
     return `${firstName}. ${lastName}`;
 }
@@ -40,4 +50,24 @@ console.log(director1)
 
 console.log(teacher3)
 
-console.log(printTeacher("John", "Doe"));
+printTeacher({ firstName: "John", lastName: "Doe" });
+
+
+class StudentClass implements StudentClassInterface{
+    firstName: string;
+    lastName: string;
+
+     constructor(firstName: string, lastName: string) {
+        this.firstName = firstName; 
+        this.lastName = lastName;    
+    }
+
+      workOnHomework(): string {
+        return "Currently working";
+    }
+
+ 
+    displayName(): string {
+        return this.firstName;
+    }
+}
