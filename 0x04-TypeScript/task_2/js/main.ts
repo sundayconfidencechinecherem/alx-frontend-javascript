a1e18f331cde78e405308b1b0d0bb802a9248ecf
sundayconfidencechinecherem <sundayconfidencechinecherem@gmail.com>
2025-11-07 19:54:50 +0100

    update

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,3 +1,5 @@
+type Subjects = 'Math' | 'History';
+
 interface DirectorInterface {
     workFromHome(): string;
     getCoffeeBreak(): string;
@@ -64,3 +66,16 @@ function executeWork(employee:Director | Teacher ): string{
 
 console.log(executeWork(createEmployee(200))); 
 console.log(executeWork(createEmployee(1000)));
+
+
+function teachClass (todayClass:Subjects): string{
+    if (todayClass === 'Math') {
+        return 'Teaching Math'
+    }
+    else{
+        return 'Teaching History'
+    }
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'))
